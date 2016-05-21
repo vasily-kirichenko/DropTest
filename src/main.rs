5e34afe08dd6a029aef5d38a0ba7f155d846d169
Vasily Kirichenko <vaskir@gmail.com>
2016-05-21 11:18:40 +0300

    move_it, borrow_it

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use std::fmt::{Display, Formatter, Result};
-
+#[derive(Debug)]
 struct My {
     field1: i32
 }
 
 impl Drop for My {
     fn drop(&mut self) {
-        println!("{} is dropping", self)    
+        println!("{:?} is dropping", self)    
     }
 }
 
-impl Display for My {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "My(field1 = {})", self.field1)
-    }
-}
+fn move_it(_my: My) {}
+
+fn borrow_it(_my: &My) {}
 
 fn main() {
-    let _my = My { field1: 1 };
-    println!("Done.")
+    let my = My { field1: 1 };
+    println!("0");
+    borrow_it(&my);
+    println!("1");
+    move_it(my);
+    println!("2")
 }
